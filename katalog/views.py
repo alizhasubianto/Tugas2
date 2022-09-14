7ea237495e8e0c86f5d10517776e5f6eda76a684
Alizha <alizha@ui.ac.id>
2022-09-14 12:18:38 +0700

    tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_catalog(request):
+    getCatalogs = CatalogItem.objects.all()
+    context = {
+        'name' : 'Aliza',
+        'student_id' : '2106652000',
+        'catalogs' : getCatalogs
+    }
+
+    return render(
+        request,
+        'katalog.html',
+        context
+    )
