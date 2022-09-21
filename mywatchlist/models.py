ed2d7376c3b3dec05e3f3caf2e5014f6502a60df
Alizha <alizha@ui.ac.id>
2022-09-21 21:41:18 +0700

    tugas 3 pbp

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class watchList(models.Model):
     watched = models.CharField(max_length = 50)
-    title = models.CharField(max_length = 50)
+    title = models.CharField(max_length = 80)
     rating = models.IntegerField()
-    release_date = models.CharField(max_length = 50)
+    release_date = models.CharField(max_length = 60)
     review = models.TextField()
--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from mywatchlist.views import show_mywatchlist
+from mywatchlist.views import show_json_by_id, show_mywatchlist, show_xml_by_id
 from mywatchlist.views import show_xml
 from mywatchlist.views import show_json
 
 app_name =  'mywatchlist'
 
 urlpatterns = [
-    path('', show_mywatchlist ,name='show_mywatchlist'),
+    path('', show_mywatchlist, name='show_mywatchlist'),
+    path('html/', show_mywatchlist, name = 'show_mywatchlist'),
     path('xml/', show_xml, name='show_xml'), 
-    path('json/', show_json, name='show_json')
+    path('json/', show_json, name='show_json'),
+    path('xml/<int:id>', show_xml_by_id, name = 'show_xml_by_id'),
+    path('json/<int:id>', show_json_by_id, name = 'show_json_by_id'),
 ]
